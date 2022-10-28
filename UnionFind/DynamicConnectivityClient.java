1403b85a6a605c69656c2b600c5083ffe9dc5012
Samya brata Maji <samyabratamaji334@gmail.com>
2022-10-28 18:12:57 +0530

    added DynamicConnectivityClient for testing

---
+++ UnionFind/DynamicConnectivityClient.java
@@ -0,0 +1,27 @@
+/**
+ * @use: for checking our union find data structure.
+ * - Reads in number of objects N from standard input.
+ * - Reads pairs of integer from standard input.
+ * - If they are not connected, then connects them.
+ */
+
+package UnionFind;
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class DynamicConnectivityClient {
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        // UF union_find = new UF(N);
+
+        // while(!StdIn.isEmpty()) {
+        //     int p = StdIn.readInt();
+        //     int q = StdIn.readInt();
+            
+        //     if (!union_find.connected(p, q)) {
+        //         union_find.union(p, q);
+        //         System.out.println("Connected "+ p +" and "+ q);
+        //     }
+        // }
+    }
+}
