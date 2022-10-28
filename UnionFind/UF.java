3de55716f8d1c355c2014893430143164d059566
Samya brata Maji <samyabratamaji334@gmail.com>
2022-10-28 18:12:10 +0530

    added UF.java class

---
+++ UnionFind/UF.java
@@ -0,0 +1,27 @@
+/**
+ * Given a set of N objects:
+ * - Union command: connects two objects
+ * - Find / Connected query: is there a path connecting two objects? 
+ */
+
+package UnionFind;
+
+public abstract class UF {
+
+    /**
+     * Initialize union find data sructure with N objects 
+     * (indexed as 0 to N-1)
+     */
+    public UF(int N) {}
+    
+    /**
+     * Adds connection between @param p and @param q.
+     */
+    public abstract void union(int p, int q);
+
+    /**
+     * Checks if @param p and @param q belong to the same
+     * connected component
+     */
+    public abstract boolean connected(int p, int q);
+}
