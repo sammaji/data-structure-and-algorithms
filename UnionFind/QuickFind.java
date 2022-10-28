71e94a328dd983b2ac8629ee7bcfa3aae9d5da73
Samya brata Maji <samyabratamaji334@gmail.com>
2022-10-28 19:28:19 +0530

    implemented quick find ds eager approach

---
+++ UnionFind/QuickFind.java
@@ -0,0 +1,31 @@
+package UnionFind;
+
+public class QuickFind extends UF {
+
+    private int N = 0;
+    private int[] entries, id = {};
+
+    public QuickFind(int N) {
+        super(N);
+        
+        this.N = N;
+        entries = new int[N];
+        for (int i=0; i < N-1; ++i) entries[i]=i;
+        id = entries.clone();
+        
+    }
+
+    @Override
+    public void union(int p, int q) {
+        int p_id =  id[p];
+        int q_id = id[q];
+        for (int i=0; i<N-1; i++) {
+            if (id[i] == q_id) id[i] = p_id;
+        }
+    }
+
+    @Override
+    public boolean connected(int p, int q) {
+        return (id[p] == id[q]);
+    }
+}
