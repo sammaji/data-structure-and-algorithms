7e5661a1bba205d982d2a747893295a57707ecda
Samya brata Maji <samyabratamaji334@gmail.com>
2022-11-03 08:35:21 +0530

    made some changes

---
+++ UnionFind/WeightedQuickUnion.java
@@ -0,0 +1,24 @@
+package UnionFind;
+
+public class WeightedQuickUnion extends UF {
+
+    public WeightedQuickUnion(int N) {
+        super(N);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void union(int p, int q) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean connected(int p, int q) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    
+    
+}
