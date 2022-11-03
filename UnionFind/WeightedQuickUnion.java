0ffbcb158b4b924943fedaf57e0f84430f962431
Samya brata Maji <samyabratamaji334@gmail.com>
2022-11-03 08:43:10 +0530

    minor changes

--- UnionFind/WeightedQuickUnion.java
+++ UnionFind/WeightedQuickUnion.java
@@ -1,10 +1,15 @@
 package UnionFind;
 
+/**
+ * Modify quick union to avoid tall trees
+ * Keep track of tree size (no. of obj. in each trees)
+ * Balance by root of smaller trees by root of larger trees
+ */
 public class WeightedQuickUnion extends UF {
 
     public WeightedQuickUnion(int N) {
         super(N);
-        //TODO Auto-generated constructor stub
+        
     }
 
     @Override
---
+++ UnionFind/WeightedQuickUnionDemo.md
@@ -0,0 +1,5 @@
+``` mermaid
+graph TD;
+    A --> B;
+    B --> C;
+```
