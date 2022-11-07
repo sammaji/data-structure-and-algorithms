b8ecedb954d55ed0a02f603541abf300920f9189
Samya brata Maji <samyabratamaji334@gmail.com>
2022-11-07 21:04:05 +0530

    added string reversing method

--- misc/easy_problems.java
+++ misc/easy_problems.java
@@ -1,13 +1,29 @@
 package misc;
 
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @question 1: Find factorial
+ * @question 2: Reverse a string in Linear Time
+ */
 public class easy_problems {
     public static void main(String[] args) {
 
     }
 
-    public static int factorial(int n) {
+    static int factorial(int n) {
         if (n==1) return 1;
         return n*factorial(n-1);
+    }
+
+    static String reverseString(@NotNull String string) {
+        String reversedString = "";
+        for (int i=string.length()-1; i>=0; i--) {
+            reversedString = reversedString.concat(
+                    String.valueOf(string.charAt(i))
+            );
+        }
+        return reversedString;
     }
 
 }
