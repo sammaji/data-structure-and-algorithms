739a58f3818b5c231318783f945ec34a8df6f8b1
Samya brata Maji <samyabratamaji334@gmail.com>
2022-11-08 19:21:17 +0530

    solved assignment problems

--- search_algo/Problems/FindCeilingInArray.java
+++ search_algo/Problems/FindCeilingInArray.java
@@ -61,4 +61,46 @@ public class FindCeilingInArray {
         }
         return -1;
     }
+
+
+    /**
+     * @Question : Leetcode 744. Find The Smallest Letter Greater Than Target
+     * You are given an array of characters letters that is sorted in non-decreasing order, and a character target.
+     * There are at least two different characters in letters.
+     * Return the smallest character in letters that is lexicographically greater than target.
+     * If such a character does not exist, return the first character in letters.
+     * Example 1:
+     * Input: letters = ["c","f","j"], target = "a"
+     * Output: "c"
+     * Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+     * Example 2:
+     * Input: letters = ["c","f","j"], target = "c"
+     * Output: "f"
+     * Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+     * Example 3:
+     * Input: letters = ["x","x","y","y"], target = "z"
+     * Output: "x"
+     * Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
+     */
+    public char nextGreatestLetter(char[] letters, char target) {
+        // two pointer approach
+        int leftPointer = 0;
+        int rightPointer = letters.length-1;
+        int midPointer;
+
+        if (letters[leftPointer]>target) return letters[leftPointer];
+        else if (letters[leftPointer] == target) return letters[1];
+
+        while(leftPointer<=rightPointer) {
+            midPointer = leftPointer + ((rightPointer-leftPointer)/2);
+            if (letters[midPointer]>target && letters[midPointer-1]<target) return letters[midPointer];
+            else if(letters[midPointer] < target) {
+                leftPointer = midPointer +1;
+            }
+            else if(letters[midPointer] > target) {
+                rightPointer = midPointer-1;
+            }
+        }
+        return letters[0];
+    }
 }
