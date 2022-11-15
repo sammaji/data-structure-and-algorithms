0fb37839af13354bc3c3613320d18ec70c71825f
Samya brata Maji <samyabratamaji334@gmail.com>
2022-11-16 00:58:06 +0530

    leetcode problem

---
+++ a1_search_algo/Problems/p06_CountNegativeNumbers.java
@@ -0,0 +1,37 @@
+package a1_search_algo.Problems;
+
+/**
+ * https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+ * Given a m x n matrix grid which is sorted in 
+ * non-increasing order both row-wise and column-wise,
+ * return the number of negative numbers in grid.
+ */
+public class p06_CountNegativeNumbers {
+    public static void main(String[] args) {
+        int[][] grid = {
+            {4,3,2,-1},
+            {3,2,1,-1},
+            {1,1,-1,-2},
+            {-1,-1,-2,-3}
+        };
+        System.out.println(
+            countNegatives(grid)
+        );  
+    }
+
+    static int countNegatives(int[][] grid) {
+        int row = 0;
+        int col = grid[0].length-1;
+        int count = 0;
+        
+        while(col >= 0 && row <= grid.length-1) {
+            if (grid[row][col] >= 0) {
+                row++;
+            } else if (grid[row][col] < 0) {
+                count += grid.length - row;
+                col--;
+            }
+        }
+        return count;
+    }
+}
