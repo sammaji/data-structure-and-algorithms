26e9f7e8e3036b9d45180fba4a407ccc6f89feaa
Samya brata Maji <samyabratamaji334@gmail.com>
2022-11-15 21:15:22 +0530

    implemented bubble sort

--- a3_sorting_algo/BubbleSort.java
+++ a3_sorting_algo/BubbleSort.java
@@ -1,8 +1,10 @@
 package a3_sorting_algo;
 
 public class BubbleSort {
+    Package a3_sorting_algo;
+    
     public static void main(String[] args) {
-        int[] arrNoRepeatition = {
+        int[] arrWithoutRepeatition = {
             1,3,4,2,6,15,17,21,9,7,20,19
         };
 
@@ -11,9 +13,27 @@ public class BubbleSort {
             21,9,7,20,19,19,21,18,6,2,
             17,15,2,17
         };
+
+        int[] result = bubbleSort(arrWithRepeatition);
+
+        for (int element : result) {
+            System.out.print(element+",");
+        }
     }
 
     static int[] bubbleSort(int[] sample) {
+        // i is the number of steps
+        // j is the index of each element in array
+        for (int i = 0; i < sample.length; i++) { 
+            for (int j = 0; j < sample.length-1; j++) {
+                if (sample[j] > sample[j+1]) {
+                    // swap
+                    int a = sample[j+1];
+                    sample[j+1] = sample[j];
+                    sample[j] = a;
+                }
+            }
+        }
         return sample;
     }
 }
