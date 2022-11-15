9ea262444c0cbf4945e0e87d019d0bc6573210b1
Samya brata Maji <samyabratamaji334@gmail.com>
2022-11-15 22:44:23 +0530

    implemented selection sort

---
+++ a3_sorting_algo/SelectionSort.java
@@ -0,0 +1,35 @@
+package a3_sorting_algo;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arrWithoutRepeatition = {
+            1,3,4,2,6,15,17,21,9,7,20,19
+        };
+
+        int[] arrWithRepeatition = {
+            15,2,1,3,4,2,6,4,15,17,
+            21,9,7,20,19,19,21,18,6,2,
+            17,15,2,17
+        };
+
+        int[] result = selectionSort(arrWithoutRepeatition);
+        for (int element : result) System.out.print(element+",");
+        System.out.println();
+        result = selectionSort(arrWithRepeatition);
+        for (int element : result) System.out.print(element+",");
+    }
+
+    static int[] selectionSort(int[] sample) {
+        for (int i = 0; i < sample.length; i++) {
+            int max_element_index = 0;
+            for (int j = 0; j < sample.length - i; j++) {
+                if (sample[j] > sample[max_element_index]) max_element_index = j;
+            }
+            // put max element towards end
+            int temp = sample[sample.length-i-1];
+            sample[sample.length-i-1] = sample[max_element_index];
+            sample[max_element_index] = temp;
+        }
+        return sample;
+    }
+}
