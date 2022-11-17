db0d0535d3a6e2fecd1e3df86b479f8fe2552492
Samya brata Maji <samyabratamaji334@gmail.com>
2022-11-17 21:20:38 +0530

    added leetcode problem

--- a2_sorting_algo/Problems/p01_MissingNumbers.java
+++ a2_sorting_algo/Problems/p01_MissingNumbers.java
@@ -1,19 +1,32 @@
 package a2_sorting_algo.Problems;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * Given an array nums containing n distinct numbers 
+ * @question: Given an array nums containing n distinct numbers 
  * in the range [0, n], return the only number in the 
  * range that is missing from the array.
+ * 
+ * @question: Given an array nums of n integers where nums[i] is in the range [1, n], 
+ * return an array of all the integers in the range [1, n] that do not appear in nums.
+ * Example 1:
+ * Input: nums = [4,3,2,7,8,2,3,1]
+ * Output: [5,6]
  */
 public class p01_MissingNumbers {
     public static void main(String[] args) {
-        int[] nums1 = {3,0,1};
-        int[] nums2 = {0,1};
-        int[] nums3 = {9,6,4,2,3,5,7,0,1};
+        int[] nums1 = {9,6,4,2,3,5,7,0,1};
+        int[] nums2 = {4,3,2,7,8,2,3,1};
 
-        System.out.println(findMissingNumbers(nums1));
-        System.out.println(findMissingNumbers(nums2));
-        System.out.println(findMissingNumbers(nums3));
+        // System.out.println(findMissingNumbers(nums1));
+        System.out.println(
+            findDisappearedNumbers(nums2)
+            );
+
+        System.out.println(Arrays.toString(nums2));
     }
 
     static int findMissingNumbers(int[] nums) {
@@ -45,4 +58,32 @@ public class p01_MissingNumbers {
 
         return index;
     }
+    
+
+    // not working
+    static List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<Integer>();
+
+        int pointer = 0;
+
+        while (pointer <= nums.length-1) {
+            if (nums[pointer] == pointer+1) pointer++;
+            else {
+                if (nums[pointer] <= nums.length) {
+                    int temp = nums[nums[pointer]-1];
+                    nums[pointer] = nums[nums[pointer]-1];
+                    nums[nums[pointer]-1] = temp;
+                } else {
+                    pointer++;
+                }
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) result.add(i);
+        }
+
+        return result;
+    }
+    
 }
